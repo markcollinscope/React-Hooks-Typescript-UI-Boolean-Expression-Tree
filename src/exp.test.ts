76e1be64ad5dff0a7ac5e71e61772cfc82f54795
Mark Collins-Cope <markcollinscope@gmail.com>
2022-02-04 23:42:55 +0000

    <master>: exp unit tests working

--- src/exp.test.ts
+++ src/exp.test.ts
@@ -1,5 +1,7 @@
 
-import * as exp from './exp'
+import {
+	AND, OR, Exp, BinExp, falseExp, trueExp, NOT, UNDEF, AppError, NoEvalError, UndefExp, NotExp, ConstExp, LB, RB, DIV, TRUE, FALSE
+} from './exp'
 
 const assert = (v: boolean)  => { if (!v) throw 'error'; }
 
@@ -8,107 +10,107 @@ describe(`test basic type creation`, function()
 {
 	it('** create exp throws app errors', function()
 	{
-		expect( () => new exp.Exp().name() ).toThrow(exp.AppError);
-		expect( () => new exp.Exp().calc() ).toThrow(exp.AppError);
+		expect( () => new Exp().name() ).toThrow(AppError);
+		expect( () => new Exp().calc() ).toThrow(AppError);
 	});
 
 	it('** undefined expression throw no-eval error on calc', function()
 	{
-		expect( () => new exp.UndefExp().calc() ).toThrow(exp.NoEvalError);
+		expect( () => new UndefExp().calc() ).toThrow(NoEvalError);
 	});
 
 	it('** true/false constants to evaluate correctly', function()
 	{
-		assert( new exp.ConstExp(true).calc() );
-		assert( ! new exp.ConstExp(false).calc() );
+		assert( new ConstExp(true).calc() );
+		assert( ! new ConstExp(false).calc() );
 	});
 
 	it('** Not (Uni) expression evaluates correctly', function()
 	{
-		assert( new exp.NotExp(new exp.ConstExp (false)).calc() );
-		assert( ! new exp.NotExp(new exp.ConstExp (true)).calc() );
+		assert( new NotExp(new ConstExp (false)).calc() );
+		assert( ! new NotExp(new ConstExp (true)).calc() );
 	});
 
 	it('** Not expression expands (to string representation) correctly', function()
 	{
-		const f = exp.falseExp;
+		const f = falseExp;
 		const fname = f.name();
 
-		const not = new exp.NotExp(f);
+		const not = new NotExp(f);
 		const notname = not.name();
 
-		assert( not.expand() === notname + exp.LB + fname + exp.RB );
+		assert( not.expand() === notname + LB + fname + RB );
 	});
 
 	it('** AND expressions evaluation correctly', function()
 	{
 
-		assert( new exp.BinExp( exp.AND, exp.trueExp, exp.trueExp ).calc() );
-		assert( ! new exp.BinExp( exp.AND, exp.trueExp, exp.falseExp ).calc() );
-		assert( ! new exp.BinExp( exp.AND, exp.falseExp, exp.trueExp ).calc() );
-		assert( ! new exp.BinExp( exp.AND, exp.falseExp, exp.falseExp ).calc() );
+		assert( new BinExp( AND, trueExp, trueExp ).calc() );
+		assert( ! new BinExp( AND, trueExp, falseExp ).calc() );
+		assert( ! new BinExp( AND, falseExp, trueExp ).calc() );
+		assert( ! new BinExp( AND, falseExp, falseExp ).calc() );
 	});
 
 	it('** OR expressions evaluation correctly', function()
 	{
-		assert( new exp.BinExp( exp.OR, exp.trueExp, exp.trueExp ).calc() );
-		assert( new exp.BinExp( exp.OR, exp.trueExp, exp.falseExp ).calc() );
-		assert( new exp.BinExp( exp.OR, exp.falseExp, exp.trueExp ).calc() );
-		assert( ! new exp.BinExp( exp.OR, exp.falseExp, exp.falseExp ).calc() );
+		assert( new BinExp( OR, trueExp, trueExp ).calc() );
+		assert( new BinExp( OR, trueExp, falseExp ).calc() );
+		assert( new BinExp( OR, falseExp, trueExp ).calc() );
+		assert( ! new BinExp( OR, falseExp, falseExp ).calc() );
 	});
 
 	it('** AND / OR expressions expand to strings correctly', function()
 	{
-		const t = exp.trueExp.name();
+		const t = trueExp.name();
 
-		assert( new exp.BinExp( exp.AND, exp.trueExp, exp.trueExp ).expand() === exp.AND + exp.LB + t + exp.DIV + t + exp.RB);
-		assert( new exp.BinExp( exp.OR, exp.trueExp, exp.trueExp ).expand() === exp.OR + exp.LB + t + exp.DIV + t + exp.RB);
+		assert( new BinExp( AND, trueExp, trueExp ).expand() === AND + LB + t + DIV + t + RB);
+		assert( new BinExp( OR, trueExp, trueExp ).expand() === OR + LB + t + DIV + t + RB);
 	});
 
 	it('** Deeper nested expression with UNDEF throws exception', function() 
 	{
 		const e = 
-			new exp.BinExp( 
-				exp.AND,
-				new exp.BinExp( 
-					exp.OR,
-					exp.trueExp,
-					exp.falseExp
+			new BinExp( 
+				AND,
+				new BinExp( 
+					OR,
+					trueExp,
+					falseExp
 				)
 				// default second arg to UndefExp ...UndefExp
 			);
 
-		expect( () => e.calc() ).toThrow(exp.NoEvalError);
+		expect( () => e.calc() ).toThrow(NoEvalError);
 	});
 	
 	it('** Deeper nested expression expands to string correctly', function() 
 	{
 		const e = 
-			new exp.BinExp( // AND 
-				exp.AND,
-				new exp.BinExp( // OR ( T, F)
-					exp.OR, 
-					exp.trueExp,
-					exp.falseExp
+			new BinExp( // AND 
+				AND,
+				new BinExp( // OR ( T, F)
+					OR, 
+					trueExp,
+					falseExp
 				),
-				new exp.NotExp( exp.falseExp ) // NOT ( F )
+				new NotExp( falseExp ) // NOT ( F )
 			);
 		
 		// AND(OR(TRUE,FALSE),NOT(FALSE))
-		assert( e.expand() === exp.AND + exp.LB + exp.OR + exp.LB + exp.TRUE + exp.DIV + exp.FALSE + exp.RB + exp.DIV + exp.NOT + exp.LB + exp.FALSE + exp.RB + exp.RB );
+		assert( e.expand() === AND + LB + OR + LB + TRUE + DIV + FALSE + RB + DIV + NOT + LB + FALSE + RB + RB );
 	});
 
 	it('** Deeper nested expression evaluates correctly', function() 
 	{
 		const e = 
-			new exp.BinExp( // TRUE
-				exp.AND,
-				new exp.BinExp( // TRUE.
-					exp.OR, 
-					exp.trueExp,
-					exp.falseExp
+			new BinExp( // TRUE
+				AND,
+				new BinExp( // TRUE.
+					OR, 
+					trueExp,
+					falseExp
 				),
-				new exp.NotExp( exp.falseExp ) // TRUE
+				new NotExp( falseExp ) // TRUE
 			);
 		assert( e.calc() );
 	}); 
