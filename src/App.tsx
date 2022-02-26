3cd0c2d550fd32d62e8ac388d53649ec40acf22d
Mark Collins-Cope <markcollinscope@gmail.com>
2022-02-26 01:55:11 +0000

    <baseline>: updated style to make mobile in landscape look ok

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { UNDEF_EXP, Exp, NotExp, uBoolToName } from './Exp'
 	dummyRoot enables the visibleRoot to be changed dynamically - something has to 'hold it' in reference form
 	for this to happen. Selecting a new option (from the dropdown menu of visibleRoot) changes the visibleRoot
 	by updating the reference held in dummyRoot (or another Exp node if deeper in Exp).
+
+	there are other possible approaches... this is a bit of a classic comp-sci solution.
 */
 
 class DummyRoot extends NotExp {}
@@ -48,7 +50,7 @@ function App()
 
 	return (
 		<div className="app">
-			<header className="app-header tal bot-margin">
+			<header className="app-header tal bot-margin lg-font">
 				De-luxe Boolean Expression Calculator
 				<p className='md-font tal'>for all your boolean evaluation needs</p>
 				<p className='md-font tal'>Thanks for upgrading - you can now use: Xor, Nand and Nor</p>
